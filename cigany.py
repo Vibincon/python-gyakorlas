8ade9d702327f54c5830847e4c1a9b4a64850a46
Skyblusama <96851516+Skyblusama@users.noreply.github.com>
2022-03-06 18:51:11 +0100

    ciklus szenvedés

---
+++ cigany.py
@@ -0,0 +1,5 @@
+cigany = input("a cigányok emberek-e?: ")
+while cigany != "nem":
+    print("cigány vagy?")
+    cigany = input("a cigányok emberek-e?: ")
+print("gratulálok tudod kik az emberek")
---
+++ ciklus.py
@@ -0,0 +1,45 @@
+i = 0
+
+while i < 5:
+    print("Amonger")
+    print(i)
+    i = i + 1
+print("Kész")
+
+gyumolcsok = ["alma", "körte", "barack", "szölö", "banán"]
+i = 0
+while i < len(gyumolcsok):
+    print(gyumolcsok[i])
+    i = i + 1
+
+baratok = ["Patrik", "Balázs", "Márk", "Lippai", "Keszthelyi", "Réti", "Pista"]
+napok = ["hétfö", "kedd", "szerda", "csütörtök",
+         "péntek", "szombat", "vasárnap"]
+i = 0
+j = 0
+while i < len(baratok):
+    print(baratok[i])
+    i = i + 1
+while j < len(napok):
+    print(napok[j])
+    j = j + 1
+
+i = 0
+j = 0
+while i < 5:
+    while j < i + 1:
+        print("*", end="")
+        j = j + 1
+    print()
+    j = 0
+    i = i + 1
+
+while i > 0:
+    while j < i-1:
+        print("*", end="")
+        j = j+1
+    print()
+    j = 0
+    i = i-1
+
+
